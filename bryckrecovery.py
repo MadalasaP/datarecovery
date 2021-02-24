f2a3a92129b8eabe1442a315175c3d9b29fa588f
Madalasa <31156187+MadalasaP@users.noreply.github.com>
2021-02-25 03:32:11 +0530

    Encryption recovery for tsutil

--- bryckrecovery.py
+++ bryckrecovery.py
@@ -20,16 +20,18 @@ class BryckRecovery(MetaStream):
             types = []
             files = os.listdir(dir_location)
             for file in files:
-                rc, msg = self.get_type(dir_location + file)
-                if rc:
-                    err_files.append(dir_location + file)
-                    err_count += 1
-                else:
-                    suc_count += 1
-                    types.append(msg)
+                if os.path.isfile(dir_location + file):
+                    rc, msg = self.get_type(dir_location + file)
+                    if rc:
+                        err_files.append(dir_location + file)
+                        err_count += 1
+                    else:
+                        suc_count += 1
+                        types.append(msg[0])
             return suc_count, err_count, err_files, types
 
         def get_type(self, file_name):
+            print(file_name)
             f = open(file_name)
             data_bytes = f.read()
             f.close()
@@ -44,7 +46,7 @@ class BryckRecovery(MetaStream):
                 if not self.validate(list(data.keys())[0],data):
                     return 1, self.msg['file_corrupt']
                 self.stream.update(data)
-                return 0, data.keys()
+                return 0, list(data.keys())
             except JSONDecodeError as err:
                 return 1, self.msg['file_corrupt']
 
